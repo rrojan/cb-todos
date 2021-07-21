58ade8ab8cf03d98e21a4b4bb7faa8934ae8b27c
rrojan20 <rrojan20@gmail.com>
2021-07-21 10:46:36 +0545

    added node binding fns for component hide and display

--- src/Tabs.js
+++ src/Tabs.js
@@ -2,6 +2,7 @@ import Tab from "./Tab";
 
 export default function Tabs(...args) {
     const tabs = document.createElement('div');
+    tabs.classList.add('d-flex', 'justify-content-center');
 
     for (let list of args) {
         const tab = Tab(list[0], list[1]);
--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@ import Header from './Header';
 import Tabs from './Tabs';
 import TodoList from './TodoList';
 
+const displayEl = (displayEl, removeEl) => {
+    displayEl.display();
+    removeEl.forEach(el => {
+        el.hide();
+    });
+}
+
 function Root() {
     const root = document.createElement('div');
-    
+
+    const todoListEl = TodoList();
+
     root.appendChild(Header());
     root.appendChild(
         Tabs(
-            ['Todos', () => { console.log('todos') }],
-            ['Completed', () => { console.log('completed') }]
+            ['Todos', () => displayEl(todoListEl, [])],
+            ['Completed', () => displayEl(todoListEl, [todoListEl])]
         )
     );
+    root.appendChild(todoListEl);
 
     return root;
 }
